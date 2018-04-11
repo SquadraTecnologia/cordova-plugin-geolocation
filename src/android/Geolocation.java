2ab938e5b3b2f5fd0eed464efa6b6ec8638517cc
Hednei Marangoni <marangonijunior@gmail.com>
2018-04-11 17:19:52 -0300

    Update

--- src/android/Geolocation.java
+++ src/android/Geolocation.java
@@ -18,9 +18,17 @@
 
 package org.apache.cordova.geolocation;
 
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.Manifest;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+
+import com.inforoeste.mocklocationdetector.MockLocationDetector;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaArgs;
@@ -37,6 +45,11 @@ public class Geolocation extends CordovaPlugin {
 
     String TAG = "GeolocationPlugin";
     CallbackContext context;
+    boolean isGPSEnabled = false;
+    boolean isNetworkEnabled = false;
+    private String LOCATION_PROVIDER = "";
+    private int MY_PERMISSIONS_REQUEST = 1000;
+    Location loc;
 
     String [] permissions = { Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION };
 
